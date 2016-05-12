a713b590243903d1b10cd8c4fbb84ea8e9c78c4e
tp6vup54 <genius091612@gmail.com>
2016-05-12 16:51:55 +0800

    complete most of the feature

--- linebot_yomao.py
+++ linebot_yomao.py
@@ -9,6 +9,7 @@ import pixiv
 config = configparser.ConfigParser()
 config.sections()
 config.read('linebot_yomao.conf')
+requests_kwargs = None
 
 WEBHOOK_PORT = int(config['Default']['webhook_port'])
 WEBHOOK_LISTEN = '0.0.0.0'
@@ -18,12 +19,18 @@ WEBHOOK_URL_PATH = config['Default']['webhook_path']
 pixiv.pixiv_username = config['Default']['pixiv_username']
 pixiv.pixiv_password = config['Default']['pixiv_password']
 
+if config.has_option('Default', 'requests_kwargs'):
+	requests_kwargs = config['Default']['requests_kwargs'].replace('\n', '')
+	requests_kwargs = eval(requests_kwargs)
+
+p = pixiv.pixiv_crewler(**requests_kwargs)
+
 LINE_ENDPOINT = "https://trialbot-api.line.me"
 
 HEADERS = {
-    "X-Line-ChannelID": config['Default']['channel_id'],
-    "X-Line-ChannelSecret": config['Default']['channel_secret'],
-    "X-Line-Trusted-User-With-ACL": config['Default']['mid']
+	"X-Line-ChannelID": config['Default']['channel_id'],
+	"X-Line-ChannelSecret": config['Default']['channel_secret'],
+	"X-Line-Trusted-User-With-ACL": config['Default']['mid']
 }
 
 app = flask.Flask(__name__)
@@ -58,6 +65,12 @@ def events(to, content):
 	r = requests.post(LINE_ENDPOINT + "/v1/events", json=data, headers=HEADERS)
 	app.logger.info(r.text)
 
+def parse_message(text):
+	arr = text.split(' ')
+	if arr[0] == '/yomao':
+		return arr[1:]
+	else:
+		return ''
 
 @app.route('/')
 def index():
@@ -69,9 +82,14 @@ def webhook():
 	app.logger.info(json_string)
 	app.logger.info(flask.request.headers)
 	update = json_string['result'][0]
-	text = update['content']['text']
-	print(text)
-	send_text([update['content']['from']], text)
+	if update["eventType"] == "138311609100106403":
+		send_text([update["from"]], '')
+	elif update["eventType"] == "138311609000106303":
+		text = update['content']['text']
+		keywords = parse_message(text)
+		if keywords and len(keywords) > 0:
+			image = p.get_image(keywords)
+			send_picture([update['content']['from']], image)
 	return ''
 
 app.run(host=WEBHOOK_LISTEN, port=WEBHOOK_PORT, debug=True)

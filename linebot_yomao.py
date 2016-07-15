d12e0c129805dd7a8f64eb5e60666cd9d3982f65
tp6vup54 <genius091612@gmail.com>
2016-07-15 23:53:37 +0800

    Modify to Simply reply version.

--- linebot_yomao.py
+++ linebot_yomao.py
@@ -4,7 +4,6 @@ import flask
 import logging
 import requests
 import configparser
-import pixiv
 
 config = configparser.ConfigParser()
 config.sections()
@@ -16,14 +15,9 @@ WEBHOOK_LISTEN = '0.0.0.0'
 
 WEBHOOK_URL_PATH = config['Default']['webhook_path']
 
-pixiv.pixiv_username = config['Default']['pixiv_username']
-pixiv.pixiv_password = config['Default']['pixiv_password']
-
 if config.has_option('Default', 'requests_kwargs'):
 	requests_kwargs = config['Default']['requests_kwargs'].replace('\n', '')
 	requests_kwargs = eval(requests_kwargs)
-
-p = pixiv.pixiv_crewler(**requests_kwargs)
 
 LINE_ENDPOINT = "https://trialbot-api.line.me"
 
@@ -65,13 +59,6 @@ def events(to, content):
 	r = requests.post(LINE_ENDPOINT + "/v1/events", json=data, headers=HEADERS)
 	app.logger.info(r.text)
 
-def parse_message(text):
-	arr = text.split(' ')
-	if arr[0] == '/yomao':
-		return arr[1:]
-	else:
-		return ''
-
 @app.route('/')
 def index():
 	return ''
@@ -86,10 +73,7 @@ def webhook():
 		send_text([update["from"]], '')
 	elif update["eventType"] == "138311609000106303":
 		text = update['content']['text']
-		keywords = parse_message(text)
-		if keywords and len(keywords) > 0:
-			image = p.get_image(keywords)
-			send_picture([update['content']['from']], image)
+		send_text([update['content']['from']], text)
 	return ''
 
 app.run(host=WEBHOOK_LISTEN, port=WEBHOOK_PORT, debug=True)
